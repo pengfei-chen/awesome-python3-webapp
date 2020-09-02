ca7d1b2e92f4a259e290b855f3ef2b255bfbb935
unknown <932564973@qq.com>
2020-09-02 19:46:38 +0800

    day_08

--- www/app.py
+++ www/app.py
@@ -33,6 +33,18 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
 
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 
 # app = web.Application(loop=loop, middlewares=[logger_factory,response_factory])
